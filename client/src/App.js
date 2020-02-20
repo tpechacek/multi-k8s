2b3023d4281fbd620ec3f293d67d3dc561db088a
Taylor Pechacek <taylortpechacek@gmail.com>
2020-02-20 20:28:22 +1100

    updated app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Fib Calculator</h1>
+          <h1 className="App-title">Fib Calculator Version 2</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
